e9c4ea6d4832bf6bd48a84b191e763a08f1c0654
soudanicode <soudanicode@gmail.com>
2026-05-30 21:37:04 +0100

    اضافة تنسيقات js

---
+++ style.js
@@ -0,0 +1,9 @@
+// scroll top  when load body
+function onLoadPage(){
+    scrollTo(0, 0)
+}
+// mega menu visible
+function visibleMenu(){
+    document.querySelector(".mega-menu").classList.add("visible-mega");   
+}
+
